d7ddb27a3c3c2b0f60c9dc51661b41fd791705ba
Shivanshu22-ui <shivasingh310102@gmail.com>
2022-04-12 09:52:24 +0530

    Stories

--- src/Stories/Stories.js
+++ src/Stories/Stories.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import Footer from '../Footer/Footer';
+import Posts from './posts/Posts';
+import Sidebar from './sidebar/Sidebar';
+
 
 function Stories() {
   return (
     <div>
+      <div className="stories d-flex">
+        <Posts/>
+        <Sidebar/>
+      </div>
         <Footer/>
     </div>
   )
