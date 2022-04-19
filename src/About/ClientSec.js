b85c6d2953ab9a89bbc34372acd610524e3dd0d7
Shivanshu22-ui <shivasingh310102@gmail.com>
2022-04-19 18:11:23 +0530

    carousel

--- src/About/ClientSec.js
+++ src/About/ClientSec.js
@@ -1,20 +1,29 @@
 import React, { Component }  from 'react'
 import "./ClientSec.css"
 import ReactDOM from 'react-dom';
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+// import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+// import { Carousel } from 'react-responsive-carousel';
 import {ClientCards} from "../Cards/Cards"
+import { Carousel } from '@trendyol-js/react-carousel';
+// import Carousel from 'carousel-react-rcdev'
 
 function clientsec() {
   return (
-    <div>
-
-<ClientCards/>
-        {/* <Carousel  >
-         
+<>
+<h2 className='text-center' >What people say about us</h2>
+      <div className="d-flex justify-content-center align-items-center"> 
+      
+      <div className='clientsCarousel'>
+        <Carousel  show={3} slide={1} swiping={true} className="d-flex justify-content-center align-items-center">
+        <ClientCards/>         
+        <ClientCards/>         
+        <ClientCards/>         
+        <ClientCards/>         
             
-        </Carousel> */}
+        </Carousel>
     </div>
+    </div>
+    </>
   )
 }
 
