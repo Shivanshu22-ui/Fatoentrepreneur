f867f09c6cfeaa3647e2051887e132d4477ab968
Shivanshu22-ui <shivasingh310102@gmail.com>
2022-04-25 00:15:20 +0530

    Places Edits

--- src/Places/PlacesInfo/PlaceDetails/PlaceDetails.js
+++ src/Places/PlacesInfo/PlaceDetails/PlaceDetails.js
@@ -1,23 +1,57 @@
-import React from 'react'
-import "./PlaceDetails.css"
-import { Carousel } from '@trendyol-js/react-carousel';
-import PlaceDetailsContent from './PlaceDetailsContent';
-
+import React from "react";
+import "./PlaceDetails.css";
+import { Carousel } from "@trendyol-js/react-carousel";
+import PlaceDetailsContent from "./PlaceDetailsContent";
+import Footer from "../../../Footer/Footer"
 
 function PlaceDetails() {
   return (
-    <div className='PlacesDetails' >
-        <Carousel show={3} slide={1} swiping={true} className="d-flex justify-content-center align-items-center placesDetailsCarousel">
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg" alt=""/>
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-6.jpg" alt="" />
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg" alt="" />
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/10/photo-1474898856510-884a2c0be546.jpeg" alt="" />
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Bar-Fisk__2018_bar_fisk_31389-1.jpg" alt="" />
-            <img src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg" alt="" />
-        </Carousel>
-<PlaceDetailsContent/>
+      <>
+    <div className="PlacesDetails">
+      <Carousel
+        show={3}
+        slide={1}
+        swiping={true}
+        autoplay={true}
+        className="d-flex justify-content-center align-items-center placesDetailsCarousel"
+      >
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg"
+          alt=""
+        />
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-6.jpg"
+          alt=""
+        />
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg"
+          alt=""
+        />
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/10/photo-1474898856510-884a2c0be546.jpeg"
+          alt=""
+        />
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Bar-Fisk__2018_bar_fisk_31389-1.jpg"
+          alt=""
+        />
+        <img
+          src="https://wp.getgolo.com/city-guide/wp-content/uploads/2019/11/Guts-Glory_Muk-van-Lil_2018_CNTraveller-GutsGlory-8.jpg"
+          alt=""
+        />
+      </Carousel>
+      <div className="container">
+        <div className="row">
+          <div className="col-7">
+            <PlaceDetailsContent />
+          </div>
+        </div>
+      </div>
+      
     </div>
-  )
+    <Footer/>
+    </>
+  );
 }
 
-export default PlaceDetails
+export default PlaceDetails;
--- src/Places/PlacesInfo/PlaceDetails/PlaceDetailsContent.js
+++ src/Places/PlacesInfo/PlaceDetails/PlaceDetailsContent.js
@@ -1,38 +1,93 @@
 import React from "react";
 import "./PlaceDetailsContent.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Accordion from "react-bootstrap/Accordion";
+import { PlacesCards } from "../../../Cards/Cards";
+
 
 function PlaceDetailsContent() {
   return (
-    <div className="container ">
-      <div className="row placeDetailsContent">
-        <div className="col-7">
-          <nav className="miniNav">
-            <ul className="d-flex my-4">
-              <li className="mx-2">Paris</li>
-              <li className="mx-2">Food & Drinks</li>
-            </ul>
-          </nav>
-          <p className="mx-2 placeName">
-            Le Servan{" "}
-            <span>
-              <i class="fa-regular fa-circle-check verified mx-3"></i>
-            </span>
-          </p>
-          <div className="d-flex place-info-details">
-            <p className="mx-2 star-review">
-              5.0{" "}
-              <span>
-                <i className="fa fa-star checked mx-1"></i>
-              </span>
-            </p>
-            <p className="mx-2">(2 reviews)</p>
-            <p className="mx-4">Free</p>
-            <p className="mx-4">Coffee Shop</p>
-          </div>
-        </div>
-        <div className="col-5">
-            
-        </div>
+    <div className="container placeDetailsContent">
+      <nav className="miniNav">
+        <ul className="d-flex my-4">
+          <li className="mx-2">Paris</li>
+          <li className="mx-2">Food & Drinks</li>
+        </ul>
+      </nav>
+      <p className="mx-2 placeName">
+        Le Servan{" "}
+        <span>
+          <i class="fa-regular fa-circle-check verified mx-3"></i>
+        </span>
+      </p>
+      <div className="d-flex place-info-details">
+        <p className="mx-2 star-review">
+          5.0{" "}
+          <span>
+            <i className="fa fa-star checked mx-1"></i>
+          </span>
+        </p>
+        <p className="mx-2">(2 reviews)</p>
+        <p className="mx-4">Free</p>
+        <p className="mx-4">Coffee Shop</p>
+      </div>
+      <hr />
+      <div className="placeServices d-flex">
+        <li className="d-flex flex-column justify-content-center align-items-center mx-4">
+          <i class="fa-solid fa-car my-2"></i>
+          <p className="my-2"> Car Parking</p>
+        </li>
+        <li className="d-flex flex-column justify-content-center align-items-center mx-4">
+          <i class="fa-brands fa-cc-mastercard my-2"></i>
+          <p className="my-2">Credit Cards</p>
+        </li>
+        <li className="d-flex flex-column justify-content-center align-items-center mx-4">
+          <i class="fa-solid fa-calendar-days my-2"></i>
+          <p className="my-2">Reservations</p>
+        </li>
+        <li className="d-flex flex-column justify-content-center align-items-center mx-4">
+          <i class="fa-solid fa-wifi my-2"></i>
+          <p className="my-2"> Wifi</p>
+        </li>
+      </div>
+      <hr />
+      <div className="showHideText">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
+          quisquam, consectetur, fugit, quae eos odit asperiores fugiat nostrum
+          numquam distinctio enim debitis cumque temporibus animi dolores autem
+          consequatur maiores neque aliquid tenetur. Consectetur, quasi cum.
+        </p>
+        .
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque
+          quisquam, consectetur, fugit, quae eos odit asperiores fugiat nostrum
+          numquam distinctio enim debitis cumque temporibus animi dolores autem
+          consequatur maiores neque aliquid tenetur. Consectetur, quasi cum.
+        </p>
+        .<p className="star-review">Show more</p>
+      </div>
+
+      <hr />
+      <div className="accordion">
+        <Accordion>
+          <Accordion.Item eventKey="0" className="my-4">
+            <Accordion.Header >What it is like to have your first date in this place?</Accordion.Header>
+            <Accordion.Body>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey="1" className="my-4">
+            <Accordion.Header>How is this business operating during COVID-19?</Accordion.Header>
+            <Accordion.Body>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+
+      <div className="similarPlaces">
+
       </div>
     </div>
   );
